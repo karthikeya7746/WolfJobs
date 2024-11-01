29416cee732fb979ef1ec27278afc6a51591ecf7
dubeavik002 <dubeavik02@gmail.com>
2024-10-31 23:02:02 -0400

    Created schema for saving the application

---
+++ backend/models/savedApplication.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const savedJobSchema = new mongoose.Schema(
+  {
+   jobId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Job',
+    required: true
+   },
+   userId:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref:"User"
+   },
+   createdAt:{
+    type: Date,
+    default: Date.now
+   }
+  },
+);
+
+const SavedJob = mongoose.model("SavedJob", savedJobSchema);
+
+module.exports = SavedJob;
