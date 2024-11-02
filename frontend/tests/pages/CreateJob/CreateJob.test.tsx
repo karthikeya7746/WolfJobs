1a3a0b80ac36e2ef76d8fbffa0939f37ce4494ad
bittubharadwaj <bittukatna@gmail.com>
2024-11-01 23:21:42 -0400

    updated createjob.test for test cases

--- frontend/tests/pages/CreateJob/CreateJob.test.tsx
+++ frontend/tests/pages/CreateJob/CreateJob.test.tsx
@@ -2,6 +2,10 @@ import { render, screen } from "@testing-library/react";
 import React from "react";
 import CreateJob from "../../../src/Pages/CreateJob/CreateJob";
 import { MemoryRouter } from "react-router";
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+
+const mock = new MockAdapter(axios);
 
 describe("CreateJob", () => {
   it("renders CreateJob", () => {
@@ -12,5 +16,35 @@ describe("CreateJob", () => {
     );
     // const headline = screen.getByText(/Hello/i);
     // expect(headline).toBeInTheDocument();
+  });
+});
+
+
+describe("CreateJob", () => {
+  beforeEach(() => {
+    // Clear prior mocks and configurations
+    mock.reset();
+  });
+
+  test("Renders CreateJob form correctly", () => {
+    render(
+      <MemoryRouter>
+        <CreateJob />
+      </MemoryRouter>
+    );
+
+  });
+
+  test("Submits the form with valid job details", async () => {
+    // Mock successful API response for job creation
+    mock.onPost("http://localhost:8000").reply(200, {
+      message: "Job created successfully",
+    });
+
+    render(
+      <MemoryRouter>
+        <CreateJob />
+      </MemoryRouter>
+    );
   });
 });
